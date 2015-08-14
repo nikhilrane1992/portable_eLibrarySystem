a215f5ba800daa9869a272d7e1517c35567b9bc6
Nikhil Rane <nikhilrane1992@gamil.com>
2015-08-15 00:21:18 +0530

    setup static and media file path

--- portable_eLibrarySystem/portable_eLibrarySystem/settings.py
+++ portable_eLibrarySystem/portable_eLibrarySystem/settings.py
@@ -102,4 +102,12 @@ TIME_ZONE = 'Asia/Kolkata'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,"deploy_to_server")
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR,"Media")
+MEDIA_URL = "/Media/"
