b34f8cdb0d8d8ef6a6f2f6a3289927572ecce81a
Nikhil Rane <nikhilrane1992@gamil.com>
2015-08-15 00:19:07 +0530

    setup time zone

--- portable_eLibrarySystem/portable_eLibrarySystem/settings.py
+++ portable_eLibrarySystem/portable_eLibrarySystem/settings.py
@@ -90,14 +90,13 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Asia/Kolkata'
 
-TIME_ZONE = 'UTC'
+# USE_I18N = True
 
-USE_I18N = True
+# USE_L10N = True
 
-USE_L10N = True
-
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
