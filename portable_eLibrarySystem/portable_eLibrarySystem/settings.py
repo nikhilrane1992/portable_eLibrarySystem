9654b135a8fa4f219f57bda41dd9c5ad010d0a6d
Nikhil Rane <nikhilrane1992@gamil.com>
2015-08-15 00:18:27 +0530

    setup database path

--- portable_eLibrarySystem/portable_eLibrarySystem/settings.py
+++ portable_eLibrarySystem/portable_eLibrarySystem/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'portable_eLibrarySystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR,"../Project_Database/eLibrarySystem"),
     }
 }
 
