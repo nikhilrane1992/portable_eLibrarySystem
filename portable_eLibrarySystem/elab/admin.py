15bcdf2e0c425b3833db92c6b50b0ac289732692
Nikhil Rane <nikhilrane1992@gamil.com>
2015-08-15 22:05:54 +0530

    create model for upload data in system

--- portable_eLibrarySystem/elab/admin.py
+++ portable_eLibrarySystem/elab/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from models import Econtaint
 
 # Register your models here.
+admin.site.register(Econtaint)
--- portable_eLibrarySystem/elab/models.py
+++ portable_eLibrarySystem/elab/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+def get_upload_containt(instance, filename):
+    return "Containt/%s" %(filename)
+
+class Econtaint(models.Model):
+	BOOK = 0
+	VIDEO = 1
+	CONTAINENTCHOICES = ((BOOK,'Book'),(VIDEO,'Video'))
+	name = models.CharField(max_length=500)
+	tag = models.IntegerField(choices=CONTAINENTCHOICES)
+	search_tags = models.TextField()
+	content = models.FileField(upload_to=get_upload_containt)
+	def __unicode__(self):
+		return self.name + ' '+'Tag-->'+self.tag
